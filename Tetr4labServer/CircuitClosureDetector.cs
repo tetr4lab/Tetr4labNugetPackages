07d6091bdb774b0f9ae283765232c1ce390c8ffb
tetr4lab. <48040768+tetr4lab@users.noreply.github.com>
2024-12-04 10:43:43 +0900

    切断検出に不要な処理を削除

--- Tetr4labServer/CircuitClosureDetector.cs
+++ Tetr4labServer/CircuitClosureDetector.cs
@@ -26,29 +26,8 @@ namespace Tetr4lab;
 /// </example>
 public class CircuitClosureDetector : CircuitHandler {
 
-    /// <summary>回路一覧</summary>
-    protected static ConcurrentDictionary<string, Circuit> Circuits { get; set; } = new ();
-
-    /// <summary>回路ID</summary>
-    protected string? Id { get; set; } = default!;
-
     /// <summary>切断時処理</summary>
     public event Action<string>? Disconnected;
-
-    /// <summary>接続時処理</summary>
-    /// <param name="circuit">回路</param>
-    /// <param name="cancellationToken">キャンセルトークン</param>
-    /// <returns></returns>
-    /// <exception cref="InvalidOperationException">サービス登録時のスコープが誤っている可能性</exception>
-    public override Task OnCircuitOpenedAsync (Circuit circuit, CancellationToken cancellationToken) {
-        //System.Diagnostics.Debug.WriteLine ($"open {Id}, {circuit.Id}");
-        if (Id is not null && Id != circuit.Id || Circuits.ContainsKey (circuit.Id)) {
-            throw new InvalidOperationException ($"already has an Id: {Id}, {circuit.Id}");
-        }
-        Id = circuit.Id;
-        Circuits [Id] = circuit;
-        return base.OnCircuitOpenedAsync (circuit, cancellationToken);
-    }
 
     /// <summary>切断時処理</summary>
     /// <param name="circuit">回路</param>
@@ -56,17 +35,12 @@ public class CircuitClosureDetector : CircuitHandler {
     /// <returns></returns>
     /// <exception cref="InvalidOperationException">サービス登録時のスコープが誤っている可能性</exception>
     public override Task OnCircuitClosedAsync (Circuit circuit, CancellationToken cancellationToken) {
-        //System.Diagnostics.Debug.WriteLine ($"close {Id}, {circuit.Id}");
-        if (Id is null || Id != circuit.Id || !Circuits.ContainsKey (circuit.Id)) {
-            throw new InvalidOperationException ($"missmatch Id: {Id}, {circuit.Id}");
-        }
         Disconnected?.Invoke (circuit.Id);
         if (Disconnected is not null) {
             foreach (Action<string> e in Disconnected.GetInvocationList ()) {
                 Disconnected -= e;
             }
         }
-        Circuits.TryRemove (circuit.Id, out var removed);
         return base.OnCircuitClosedAsync (circuit, cancellationToken);
     }
 }

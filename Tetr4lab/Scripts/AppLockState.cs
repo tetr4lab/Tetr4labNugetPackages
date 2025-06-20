e172ef036d85f51e14cbbed24adc5f25d056874a
tetr4lab. <48040768+tetr4lab@users.noreply.github.com>
2025-06-21 01:58:51 +0900

    Uiロック、アプリモード

---
+++ Tetr4lab/Scripts/AppLockState.cs
@@ -0,0 +1,174 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Tetr4lab;
+
+/// <summary>UIロックを管理する</summary>
+/// <remarks><example>Program.cs でのサービス登録<code>
+/// builder.Services.AddScoped&lt;<see cref="IAppLockState"/>, <see cref="AppLockState"/>&gt; ();
+/// </code></example></remarks>
+public interface IAppLockState {
+    /// <summary>プロパティの変更を通知するイベント</summary>
+    /// <remarks><example>使用例<code>
+    /// @implements IDisposable
+    /// @inject IAppLockState AppLockState
+    /// @code {
+    ///     // イベントハンドラ
+    ///     protected void OnAppLockPropertyChanged (object? sender, PropertyChangedEventArgs e) {
+    ///         InvokeAsync (StateHasChanged); // 再描画
+    ///     }
+    ///     protected override void OnInitialized () {
+    ///         base.OnInitialized ();
+    ///         AppLockState.PropertyChanged += OnAppLockPropertyChanged; // 購読開始
+    ///     }
+    ///     public void Dispose () {
+    ///         AppLockState.PropertyChanged -= OnAppLockPropertyChanged; // 購読終了
+    ///     }
+    /// }
+    /// </code></example></remarks>
+    public event PropertyChangedEventHandler? PropertyChanged;
+    /// <summary>現在操作不能である</summary>
+    public bool IsLocked { get; }
+    /// <summary>操作不能状態の理由</summary>
+    public string Reason { get; }
+    /// <summary>目標値</summary>
+    public int TotalProgressValue { get; }
+    /// <summary>現在値</summary>
+    public int CurrentProgressValue { get; }
+    /// <summary>ロック状態にする</summary>
+    /// <param name="reason">ロックの理由</param>
+    /// <param name="totalProgressValue">進捗の完了目標値</param>
+    public void Lock (string reason, int totalProgressValue);
+    /// <summary>ロック状態にする</summary>
+    /// <param name="reason">ロックの理由</param>
+    public void Lock (string reason);
+    /// <summary>ロック状態にする</summary>
+    /// <param name="totalProgressValue">進捗の完了目標値</param>
+    public void Lock (int totalProgressValue);
+    /// <summary>ロック状態にする</summary>
+    public void Lock ();
+    /// <summary>ロック状態を解除する</summary>
+    public void Unlock ();
+    /// <summary>進捗の現在値を更新</summary>
+    /// <param name="value">新しい現在値</param>
+    public void UpdateProgress (int value);
+    /// <summary>進捗率</summary>
+    public double ProgressPercentage { get; }
+}
+
+/// <summary>UIロックを管理する</summary>
+public class AppLockState : IAppLockState, INotifyPropertyChanged {
+    /// <summary>プロパティの変更を通知するイベント</summary>
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    /// <summary>プロパティの変更を通知するヘルパーメソッド</summary>
+    /// <param name="propertyName"></param>
+    protected void OnPropertyChanged ([CallerMemberName] string? propertyName = null) {
+        PropertyChanged?.Invoke (this, new PropertyChangedEventArgs (propertyName));
+    }
+
+    /// <summary>現在操作不能である</summary>
+    public bool IsLocked {
+        get => _isLocked;
+        protected set {
+            if (_isLocked != value) {
+                _isLocked = value;
+                OnPropertyChanged ();
+            }
+        }
+    }
+    /// <summary>操作不能の内部値</summary>
+    protected bool _isLocked;
+
+    /// <summary>操作不能状態の理由</summary>
+    public string Reason {
+        get => _reason;
+        protected set {
+            if (_reason != value) {
+                _reason = value;
+                OnPropertyChanged ();
+            }
+        }
+    }
+    /// <summary>理由の内部値</summary>
+    protected string _reason = string.Empty;
+
+    /// <summary>目標値</summary>
+    public int TotalProgressValue {
+        get => _totalProgress;
+        protected set {
+            if (_totalProgress != value) {
+                _totalProgress = value;
+                OnPropertyChanged ();
+                OnPropertyChanged (nameof (ProgressPercentage));
+            }
+        }
+    }
+    /// <summary>目標内部値</summary>
+    protected int _totalProgress;
+
+    /// <summary>現在値</summary>
+    public int CurrentProgressValue {
+        get => _currentProgress;
+        protected set {
+            if (_currentProgress != value) {
+                _currentProgress = value;
+                OnPropertyChanged ();
+                OnPropertyChanged (nameof (ProgressPercentage));
+            }
+        }
+    }
+    /// <summary>現在内部値</summary>
+    protected int _currentProgress;
+
+    /// <summary>ロック状態にする</summary>
+    /// <param name="reason">ロックの理由</param>
+    /// <param name="totalProgressValue">進捗の完了目標値</param>
+    public void Lock (string reason, int totalProgressValue) {
+        IsLocked = true;
+        Reason = reason;
+        TotalProgressValue = totalProgressValue < 0 ? 0 : totalProgressValue;
+        CurrentProgressValue = 0;
+    }
+
+    /// <summary>ロック状態にする</summary>
+    /// <param name="reason">ロックの理由</param>
+    public void Lock (string reason) {
+        IsLocked = true;
+        Reason = reason;
+        TotalProgressValue = CurrentProgressValue = 0;
+    }
+
+    /// <summary>ロック状態にする</summary>
+    /// <param name="totalProgressValue">進捗の完了目標値</param>
+    public void Lock (int totalProgressValue) {
+        IsLocked = true;
+        Reason = string.Empty;
+        TotalProgressValue = totalProgressValue < 0 ? 0 : totalProgressValue;
+        CurrentProgressValue = 0;
+    }
+
+    /// <summary>ロック状態にする</summary>
+    public void Lock () {
+        IsLocked = true;
+        Reason = string.Empty;
+        TotalProgressValue = CurrentProgressValue = 0;
+    }
+
+    /// <summary>ロック状態を解除する</summary>
+    public void Unlock () {
+        IsLocked = false;
+        Reason = string.Empty;
+        TotalProgressValue = 0;
+        CurrentProgressValue = 0;
+    }
+
+    /// <summary>進捗の現在値を更新</summary>
+    /// <param name="value">新しい現在値</param>
+    public void UpdateProgress (int value) {
+        CurrentProgressValue = value < 0 ? 0 : value > TotalProgressValue ? TotalProgressValue : value;
+    }
+
+    /// <summary>進捗率</summary>
+    public double ProgressPercentage => 100d * CurrentProgressValue / TotalProgressValue;
+}
---
+++ Tetr4lab/Scripts/AppModeService.cs
@@ -0,0 +1,117 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Tetr4lab;
+
+/// <summary>アプリモード管理</summary>
+/// <remarks><example>Program.cs でのサービス登録<code>
+/// builder.Services.AddScoped&lt;<see cref="IAppModeService&lt;TEnum&gt;"/>, <see cref="AppModeService&lt;TEnum&gt;"/>&gt; ();
+/// </code></example></remarks>
+public interface IAppModeService<TEnum> where TEnum : Enum {
+    /// <summary>プロパティの変更を通知するイベント</summary>
+    /// <remarks><example>使用例<code>
+    /// // アプリのモード
+    /// public enum AppMode {
+    ///     None = AppModeService&lt;AppMode&gt;.NoneMode,
+    ///     Boot = AppModeService&lt;AppMode&gt;.DefaultMode,
+    ///     List,
+    ///     Detail,
+    ///     Settings,
+    /// }
+    /// </code><code>
+    /// @implements IDisposable
+    /// @inject IAppModeService&lt;AppMode&gt; AppModeService
+    /// @code {
+    ///     // イベントハンドラ
+    ///     protected void OnAppModePropertyChanged (object? sender, PropertyChangedEventArgs e) {
+    ///         if (e.PropertyName == &quot;CurrentMode&quot;) {
+    ///             InvokeAsync (StateHasChanged); // モード変更による再描画
+    ///         }
+    ///         if (e.PropertyName == &quot;RequestedMode&quot; &amp;&amp; sender is IAppModeService&lt;AppMode&gt; service) {
+    ///             if (service.RequestedMode != AppMode.None &amp;&amp; service.RequestedMode != service.CurrentMode) {
+    ///                 if (true /*or 可否判断*/) {
+    ///                     service.SetMode (service.RequestedMode); // モード変更要求を受け付けて実際に変更
+    ///                 }
+    ///                 service.SetRequestedMode (AppMode.None);
+    ///             }
+    ///         }
+    ///     }
+    ///     protected override void OnInitialized () {
+    ///         base.OnInitialized ();
+    ///         AppModeService.PropertyChanged += OnAppModePropertyChanged; // 購読開始
+    ///     }
+    ///     public void Dispose () {
+    ///         AppModeService.PropertyChanged -= OnAppModePropertyChanged; // 購読終了
+    ///     }
+    /// }
+    /// </code></example></remarks>
+    event PropertyChangedEventHandler? PropertyChanged;
+    /// <summary>現在のモード</summary>
+    TEnum CurrentMode { get; }
+    /// <summary>要求されたモード</summary>
+    /// <remarks>See: <see cref="PropertyChanged"/></remarks>
+    TEnum RequestedMode { get; }
+    /// <summary>モードを設定</summary>
+    void SetMode (TEnum mode);
+    /// <summary>モードを要求</summary>
+    /// <remarks>See: <see cref="PropertyChanged"/></remarks>
+    void SetRequestedMode (TEnum mode);
+}
+
+    /// <summary>アプリモード管理</summary>
+public class AppModeService<TEnum> : IAppModeService<TEnum>, INotifyPropertyChanged where TEnum : Enum {
+
+    /// <summary>抹消番号</summary>
+    public const int NoneMode = -1;
+
+    /// <summary>初期番号</summary>
+    public const int DefaultMode = 0;
+
+    /// <summary>プロパティの変更を通知するイベント</summary>
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    /// <summary>プロパティの変更を通知するヘルパーメソッド</summary>
+    /// <param name="propertyName">変更されたプロパティとして通知に含める名前 (省略時は元の名前)</param>
+    protected void OnPropertyChanged ([CallerMemberName] string? propertyName = null) {
+        PropertyChanged?.Invoke (this, new PropertyChangedEventArgs (propertyName));
+    }
+
+    /// <summary>アプリのモード</summary>
+    public TEnum CurrentMode {
+        get => _currentMode;
+        protected set {
+            if (!_currentMode.Equals (value)) {
+                _currentMode = value;
+                OnPropertyChanged ();
+            }
+        }
+    }
+    /// <summary>現在のモード内部値</summary>
+    protected TEnum _currentMode = (TEnum) (object) DefaultMode;
+
+    /// <summary>リクエストされたアプリモード</summary>
+    public TEnum RequestedMode {
+        get => _requestedMode;
+        protected set {
+            if (!_requestedMode.Equals (value)) {
+                _requestedMode = value;
+                OnPropertyChanged ();
+            }
+        }
+    }
+    /// <summary>要求されたモード内部値</summary>
+    protected TEnum _requestedMode = (TEnum) (object) NoneMode;
+
+    /// <summary>モードを設定</summary>
+    /// <param name="mode">新しいモード</param>
+    public void SetMode (TEnum mode) {
+        CurrentMode = mode;
+    }
+
+    /// <summary>モードをリクエスト</summary>
+    /// <param name="mode">要求するモード</param>
+    public void SetRequestedMode (TEnum mode) {
+        RequestedMode = mode;
+    }
+}
+

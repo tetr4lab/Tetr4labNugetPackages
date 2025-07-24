cc4b9a2df06f49b08c891960006df8ed5bfc77de
tetr4lab. <48040768+tetr4lab@users.noreply.github.com>
2025-07-24 11:02:10 +0900

    MySql接続文字列合成

---
+++ Tetr4labServer/ConfigurationManagerHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Tetr4lab;
+
+/// <summary>アプリ構成マネジャ拡張</summary>
+public static partial class ConfigurationManagerHelper {
+    /// <summary>MySql接続文字列合成</summary>
+    /// <param name="configuration"></param>
+    /// <param name="database"></param>
+    /// <returns></returns>
+    public static string MyConnectionString (this ConfigurationManager configuration, string database)
+        => $"database={database};{configuration.GetConnectionString ("Host")}{configuration.GetConnectionString ("Account")}Allow User Variables=true;";
+}

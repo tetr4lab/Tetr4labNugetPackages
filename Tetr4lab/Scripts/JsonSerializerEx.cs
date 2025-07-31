11e00525c0258568b1eefdd5d52d0f5e8f8fadbf
tetr4lab. <48040768+tetr4lab@users.noreply.github.com>
2025-07-31 18:43:15 +0900

    JsonSerializerEx(verbatim stringed json)

---
+++ Tetr4lab/Scripts/JsonSerializerEx.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using System.Text.RegularExpressions;
+
+namespace Tetr4lab;
+
+/// <summary>JsonSerializeer拡張</summary>
+public static class JsonSerializerEx {
+    /// <summary>逐語的テキストを使用したJsonをパース</summary>
+    /// <typeparam name="TItem"></typeparam>
+    /// <param name="atJson">`@""`を使えるJson</param>
+    /// <returns></returns>
+    public static TItem? Deserialize<TItem> (string atJson)
+        => JsonSerializer.Deserialize<TItem> (
+            new Regex (@"@""((?:[^""]|"""")*)""(?!"")", RegexOptions.Singleline)
+            .Replace (atJson, m => $"\"{m.Groups [1].Value.Replace ("\"\"", "\"").Replace ("\\", "\\\\")}\"")
+        );
+}

c4db1a0add2ca13a9d8f4b5792d89d57e7e4c7c7
tetr4lab. <48040768+tetr4lab@users.noreply.github.com>
2025-08-06 12:01:59 +0900

    MudBlazor 8.11.0, 名前によるアイコン生成

---
+++ Tetr4labRazor/MudIconsHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using MudBlazor;
+
+namespace Tetr4lab;
+
+/// <summary>MudBlazor Icons拡張</summary>
+public static class MudIconsHelper {
+    /// <summary>動的アイコン生成</summary>
+    /// <param name="name">アイコン名(@"((?:\w+\.){0,3}\w+)")</param>
+    /// <returns>アイコン</returns>
+    public static string GetIcon (this string name) {
+        var parts = name.Split ('.');
+        var iconField = typeof (Icons)
+            .GetNestedType (parts.Length > 2 ? parts [^3] : "Material")?
+            .GetNestedType (parts.Length > 1 ? parts [^2] : "Filled")?
+            .GetField (parts [^1], BindingFlags.Public | BindingFlags.Static);
+        if (iconField is not null) {
+            return (string?) iconField.GetValue (null) ?? string.Empty;
+        }
+        return Icons.Material.Filled.Error;
+    }
+}

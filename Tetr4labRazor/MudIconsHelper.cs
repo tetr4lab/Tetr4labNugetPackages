10f4f55557349d4a90e040a0471d084f8ac0c960
tetr4lab. <48040768+tetr4lab@users.noreply.github.com>
2025-08-06 12:38:14 +0900

    名前によるアイコン生成の調整

--- Tetr4labRazor/MudIconsHelper.cs
+++ Tetr4labRazor/MudIconsHelper.cs
@@ -5,17 +5,18 @@ namespace Tetr4lab;
 
 /// <summary>MudBlazor Icons拡張</summary>
 public static class MudIconsHelper {
-    /// <summary>動的アイコン生成</summary>
+    /// <summary>アイコンを名前で取得</summary>
     /// <param name="name">アイコン名(@"((?:\w+\.){0,3}\w+)")</param>
     /// <returns>アイコン</returns>
     public static string GetIcon (this string name) {
         var parts = name.Split ('.');
-        var iconField = typeof (Icons)
+        var iconValue = typeof (Icons)
             .GetNestedType (parts.Length > 2 ? parts [^3] : "Material")?
             .GetNestedType (parts.Length > 1 ? parts [^2] : "Filled")?
-            .GetField (parts [^1], BindingFlags.Public | BindingFlags.Static);
-        if (iconField is not null) {
-            return (string?) iconField.GetValue (null) ?? string.Empty;
+            .GetField (parts [^1], BindingFlags.Public | BindingFlags.Static)?
+            .GetValue (null);
+        if (iconValue is not null) {
+            return (string) iconValue;
         }
         return Icons.Material.Filled.Error;
     }

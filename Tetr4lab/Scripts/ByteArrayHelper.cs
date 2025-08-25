90e9d965aeb5b410d4855cfc2a870931fa024b7b
tetr4lab. <48040768+tetr4lab@users.noreply.github.com>
2025-08-25 14:20:18 +0900

    metaファイルを追加、C#9対応

--- Tetr4lab/Scripts/ByteArrayHelper.cs
+++ Tetr4lab/Scripts/ByteArrayHelper.cs
@@ -1,34 +1,36 @@
-﻿namespace Tetr4lab;
+﻿namespace Tetr4lab {
 
-/// <summary>Byte配列拡張</summary>
-public static class ByteArrayHelper {
-    /// <summary>画像種別を判定</summary>
-    /// <param name="image">画像</param>
-    /// <returns>MIMEサブタイプまたは`null`</returns>
-    public static string? DetectImageType (this byte []? image) {
-        if (image is not null) {
-            var header = BitConverter.ToString (image [0..12]).Replace ("-", "");
-            if (header == "89504E470D0A1A0A") {
-                return "png";
+    /// <summary>Byte配列拡張</summary>
+    public static class ByteArrayHelper {
+        /// <summary>画像種別を判定</summary>
+        /// <param name="image">画像</param>
+        /// <returns>MIMEサブタイプまたは`null`</returns>
+        public static string? DetectImageType (this byte []? image) {
+            if (image is not null) {
+                var header = BitConverter.ToString (image [0..12]).Replace ("-", "");
+                if (header == "89504E470D0A1A0A") {
+                    return "png";
+                }
+                if (header.StartsWith ("FFD8FFE") && header.Length >= 8 && "01238E".Contains (header [7])) {
+                    return "jpeg";
+                }
+                if (header.StartsWith ("474946383761") || header.StartsWith ("474946383961")) {
+                    return "gif";
+                }
+                if (header.StartsWith ("3C3F786D") || header.StartsWith ("3C737667")) {
+                    return "svg+xml";
+                }
+                if (header.StartsWith ("524946463A") && header [16..24] == "57454250") {
+                    return "webp";
+                }
             }
-            if (header.StartsWith ("FFD8FFE") && header.Length >= 8 && "01238E".Contains (header [7])) {
-                return "jpeg";
-            }
-            if (header.StartsWith ("474946383761") || header.StartsWith ("474946383961")) {
-                return "gif";
-            }
-            if (header.StartsWith ("3C3F786D") || header.StartsWith ("3C737667")) {
-                return "svg+xml";
-            }
-            if (header.StartsWith ("524946463A") && header [16..24] == "57454250") {
-                return "webp";
-            }
+            return null;
         }
-        return null;
+        /// <summary>埋め込み画像データ</summary>
+        /// <param name="image">画像</param>
+        /// <returns>データまたは空文字列</returns>
+        public static string ToImageSource (this byte []? image)
+            => image is null || image.Length < 8 ? string.Empty : $"data:image/{image.DetectImageType () ?? "jpeg"};base64,{Convert.ToBase64String (image)}";
     }
-    /// <summary>埋め込み画像データ</summary>
-    /// <param name="image">画像</param>
-    /// <returns>データまたは空文字列</returns>
-    public static string ToImageSource (this byte []? image)
-        => image is null || image.Length < 8 ? string.Empty : $"data:image/{image.DetectImageType () ?? "jpeg"};base64,{Convert.ToBase64String (image)}";
+
 }
--- Tetr4lab/Scripts/NaturalSort.cs
+++ Tetr4lab/Scripts/NaturalSort.cs
@@ -1,43 +1,45 @@
 ﻿using System.Text.RegularExpressions;
 
-namespace Tetr4lab;
+namespace Tetr4lab {
 
-/// <summary>自然順ソート</summary>
-public static class NaturalSortHelper {
-    /// <summary>自然順ソート用比較</summary>
-    private static IComparer<string> _naturalSortComparer = new NaturalSortComparer ();
     /// <summary>自然順ソート</summary>
-    /// <param name="list">対象</param>
-    public static void NaturalSort (this List<string> list)
-        => list.Sort (_naturalSortComparer.Compare);
-    /// <summary>自然順ソート</summary>
-    /// <param name="array">対象</param>
-    public static void NaturalSort (this string [] array)
-        => Array.Sort (array, _naturalSortComparer.Compare);
-}
-
-/// <summary>自然順ソート用比較</summary>
-public class NaturalSortComparer : IComparer<string> {
-    /// <summary>自然順ソート用セパレータ</summary>
-    private static readonly Regex _separator = new (@"(-?[\d\,]+(\.\d+)?)", RegexOptions.Compiled);
-    /// <summary>自然順ソート用比較</summary>
-    /// <param name="x">対象1</param>
-    /// <param name="y">対象2</param>
-    public int Compare (string? x, string? y) {
-        if (x is null && y is null) { return 0; }
-        if (x is null) { return -1; }
-        if (y is null) { return 1; }
-        var xParts = _separator.Split (x);
-        var yParts = _separator.Split (y);
-        var minLength = Math.Min (xParts.Length, yParts.Length);
-        for (var i = 0; i < minLength; i++) {
-            var comparison = decimal.TryParse (xParts [i], out var xNum) && decimal.TryParse (yParts [i], out var yNum)
-                ? xNum.CompareTo (yNum)
-                : string.Compare (xParts [i], yParts [i], StringComparison.OrdinalIgnoreCase);
-            if (comparison != 0) {
-                return comparison;
-            }
-        }
-        return x.Length.CompareTo (y.Length);
+    public static class NaturalSortHelper {
+        /// <summary>自然順ソート用比較</summary>
+        private static IComparer<string> _naturalSortComparer = new NaturalSortComparer ();
+        /// <summary>自然順ソート</summary>
+        /// <param name="list">対象</param>
+        public static void NaturalSort (this List<string> list)
+            => list.Sort (_naturalSortComparer.Compare);
+        /// <summary>自然順ソート</summary>
+        /// <param name="array">対象</param>
+        public static void NaturalSort (this string [] array)
+            => Array.Sort (array, _naturalSortComparer.Compare);
     }
+
+    /// <summary>自然順ソート用比較</summary>
+    public class NaturalSortComparer : IComparer<string> {
+        /// <summary>自然順ソート用セパレータ</summary>
+        private static readonly Regex _separator = new (@"(-?[\d\,]+(\.\d+)?)", RegexOptions.Compiled);
+        /// <summary>自然順ソート用比較</summary>
+        /// <param name="x">対象1</param>
+        /// <param name="y">対象2</param>
+        public int Compare (string? x, string? y) {
+            if (x is null && y is null) { return 0; }
+            if (x is null) { return -1; }
+            if (y is null) { return 1; }
+            var xParts = _separator.Split (x);
+            var yParts = _separator.Split (y);
+            var minLength = Math.Min (xParts.Length, yParts.Length);
+            for (var i = 0; i < minLength; i++) {
+                var comparison = decimal.TryParse (xParts [i], out var xNum) && decimal.TryParse (yParts [i], out var yNum)
+                    ? xNum.CompareTo (yNum)
+                    : string.Compare (xParts [i], yParts [i], StringComparison.OrdinalIgnoreCase);
+                if (comparison != 0) {
+                    return comparison;
+                }
+            }
+            return x.Length.CompareTo (y.Length);
+        }
+    }
+
 }

bf8b6e7b8a1e41bcf4a187a4960d0725e512eca5
tetr4lab. <48040768+tetr4lab@users.noreply.github.com>
2026-05-31 17:37:40 +0900

    指定時間帯に強制遷移を起こしてサーバから切断する

---
+++ Tetr4labRazor/ShutdownTimer.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+namespace Tetr4lab;
+
+/// <summary>毎日指定時間帯に指定ページへ遷移する</summary>
+/// <remarks>開きっぱなしのタブを指定ページに追い出す</remarks>
+public partial class ShutdownTimer : ComponentBase {
+    /// <summary>遷移開始時刻</summary>
+    [Parameter] public TimeOnly StartTime { get; set; } = new TimeOnly (6, 0, 0);
+    /// <summary>遷移終了時刻</summary>
+    [Parameter] public TimeOnly EndTime { get; set; } = new TimeOnly (6, 10, 0);
+    /// <summary>時刻確認間隔 (ms)</summary>
+    [Parameter] public int Timertick { get; set; } = 300000;
+    /// <summary>遷移先URL</summary>
+    [Parameter] public string TargetUrl { get; set; } = "/";
+}
